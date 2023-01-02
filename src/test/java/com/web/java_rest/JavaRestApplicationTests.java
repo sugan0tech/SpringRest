9050ad0ba9c3a4533fab34f306ab5f8ee3c3babb
sugan0tech <sugankpms@gmail.com>
2023-01-02 19:29:45 +0530

    initial commit

---
+++ src/test/java/com/web/java_rest/JavaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.java_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
